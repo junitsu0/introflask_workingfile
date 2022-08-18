bebfbfdb28f60db736e02d259fa43bb0b0382fc2
Justin <junitsu00@gmail.com>
2022-08-18 14:49:09 -0500

    d2am

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+from app.forms import SignUpForm
+from app.forms import LogInForm
 
 
 @app.route('/')
@@ -9,10 +11,14 @@ def index():
         'email': 'brians@codingtemple.com'
     }
     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-    return render_template('index.html', user=user_info,
-    colors=colors)
+    return render_template('index.html', user=user_info, colors=colors)
 
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
 
-@app.route('/test')
-def test():
-    return 'This is a test'
+@app.route('/login')
+def login():
+    form = LogInForm()
+    return render_template('login.html', form=form)
