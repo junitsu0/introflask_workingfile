b8be3485c78cdf37d53ff09c77774e07714171a2
Justin <junitsu00@gmail.com>
2022-08-21 15:55:15 -0500

    login manager

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -11,6 +12,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 # Create an instance of Migrate which will be out migration engine and pass in the app and SQLAlchemy instance
 migrate = Migrate(app, db)
+# Create an instance of the LoginManager to handle authentication
+login = LoginManager(app)
 
 
 from . import routes, models
